f38b0a5e7fff46dc4bf37ad6f451e29cce741c0c
alexfeerre <afernandez5235@esdmadrid.es>
2024-01-12 20:39:13 +0100

    new

--- app/article.js
+++ app/article.js
@@ -15,8 +15,8 @@ window.onload = () => {
           let imagesHTML = '';
           article.imagenes.split(',').forEach((imageUrl, index) => {
             const imgKey = `desc_img${index + 1}`;
-            const imgDesc = article[imgKey].trim();
-            if (imageUrl.trim() !== '') {
+            const imgDesc = article[imgKey]?.trim();
+            if (imageUrl.trim() !== '' && imgDesc) {
               imagesHTML += `
                 <div class="image-container">
                   <img class="article-images-details" src="${imageUrl.trim()}" alt="${imgDesc}">
@@ -28,36 +28,93 @@ window.onload = () => {
           return imagesHTML;
         };
 
-        // Crear la estructura HTML con todos los datos del artículo
         const articleHTML = `
-          <h1>${article.titulo}</h1>
-          <p>Marca temporal: ${article.marca_temporal}</p>
-          <p>Correo del docente: ${article.correo_docente}</p>
-          <p>Nombre del estudiante: ${article.nombre_estudiante}</p>
-          <p>Redes del estudiante: ${article.redes_estudiante}</p>
-          <p>Título: ${article.titulo}</p>
-          <p>Subtítulo: ${article.subtitulo}</p>
-          <p>Descripción: ${article.descripcion}</p>
-          <p>Palabras clave: ${article.palabras_clave}</p>
-          <p>Línea de investigación: ${article.linea_investigacion}</p>
-          <p>Especialidad: ${article.especialidad}</p>
-          <p>Asignatura: ${article.asignatura}</p>
-          <p>Curso: ${article.curso}</p>
-          <p>Entidad: ${article.entidad}</p>
+          <div class="introdetail">
+          <p class="timedetail">${article.marca_temporal ? `Publicado el ${article.marca_temporal}` : ''}</p>
+          <h1 class="titledetail">${article.titulo ? `${article.titulo}` : 'Título No especificado'}</h1>
+          <p class="subtitledetail">${article.subtitulo ? `${article.subtitulo}` : 'Subtítulo No especificado'}</p>
+          <div class="subclassestitulo">
+          <p class="namedetail">${article.nombre_estudiante ? `${article.nombre_estudiante}` : 'Nombre del estudiante no especificado'}</p>
+          <p class="classdetail">${article.asignatura ? `Asignatura: ${article.asignatura}` : 'Asignatura no especificada'}</p>
+          <p class="modaldetail">${article.especialidad ? `Especialidad: ${article.especialidad}` : 'Especialidad no especificada'}</p>
+          </div>
+          </div>
+          <section class="datadetails">
+          <p class="entity">${article.entidad ? `<p class="entidaddetail">Entidad: ${article.entidad}` : 'Entidad no especificada'}</p>
+          <p class="linedetail">${article.linea_investigacion ? `Línea de investigación: ${article.linea_investigacion}` : 'Línea de investigación no especificada'}</p>
+          <p class="maildetail">${article.correo_docente ? `Correo del docente: ${article.correo_docente}` : 'Correo del docente no especificado'}</p>
+          <p class="descriptiondetail" >${article.descripcion ? `Descripción: ${article.descripcion}` : 'Descripción no especificada'}</p>
+          <p class="keydetail">${article.palabras_clave ? `Palabras clave: ${article.palabras_clave}` : 'Palabras clave no especificadas'}</p>
+          <p class="cursodetail">${article.curso ? `Curso: ${article.curso}` : 'Curso no especificado'}</p>
           <div class="article-images">
             ${renderImages()}
           </div>
-          <p>doc_info: ${article.doc_info}</p>
-          <p>form2: ${article.form2}</p>
-          <p>Enlace video: ${article.enlace_video}</p>
-          <p>Enlace Info: ${article.enlace_info}</p>
-
+          <div class="social">
+          <p class="socialtitle">Redes Sociales del estudiante</p>
+          <p class="socialdetail">${article.redes_estudiante ? `${article.redes_estudiante}` : 'Redes del estudiante no especificadas'}</p>
+          </div>
+          <div class="enlaces_details">
+          ${article.enlace_video ? `<button class="link-button" onclick="window.location.href='${article.enlace_video}'">Enlace video</button>` : ''}
+          ${article.enlace_info ? `<button class="link-button" onclick="window.location.href='${article.enlace_info}'">Enlace Info</button>` : ''}
+          ${article.doc_info ? `<button class="link-button" onclick="window.location.href='${article.doc_info}'">Enlace Documento</button>` : ''}
+          ${article.form2 ? `<button class="link-button" onclick="window.location.href='${article.form2}'">Enlace Formulario 2</button>` : ''}
+          </div>
+          </section>
         `;
 
         articleDetails.innerHTML = articleHTML;
+
+        // Agregar estilos para los botones
+        const linkButtons = document.querySelectorAll('.link-button');
+        linkButtons.forEach(button => {
+          button.style.cursor = 'pointer';
+          button.style.backgroundColor = 'black';
+          button.style.color = 'white';
+          button.style.padding = '2% 5% 2% 5%';
+          button.style.border = 'none';
+        
+        });
       } else {
         articleDetails.innerHTML = "<p>Artículo no encontrado.</p>";
       }
     })
     .catch(error => console.error('Error al cargar los detalles del artículo:', error));
+
+  // ENVIAR CORREO
+  const emailInput = document.getElementById('emailInput');
+  const enviarCorreoBtn = document.getElementById('enviarCorreoBtn');
+  let mensajeMostrado = false;
+
+  function enviarCorreo() {
+    const mensaje = document.getElementById('mensaje');
+    const correo = emailInput.value.trim().toLowerCase();
+    const dominiosPermitidos = ['@esdmadrid.es', '@educamadrid.org', '@hotmail.com', '@gmail.com'];
+
+    let valido = dominiosPermitidos.some(dominio => correo.endsWith(dominio));
+
+    if (valido) {
+      mensaje.textContent = `Correo enviado a ${correo}.`;
+      mensaje.style.color = 'white';
+      emailInput.value = ''; 
+    } else {
+      mensaje.textContent = 'Por favor, ingresa una dirección de correo válida.';
+      mensaje.style.color = 'white';
+    }
+
+    mensaje.style.opacity = '1';
+
+    setTimeout(() => {
+      mensaje.style.opacity = '0';
+      mensaje.style.transition = 'opacity 1s ease-in-out';
+    }, 5000);
+  }
+
+  enviarCorreoBtn.addEventListener('click', enviarCorreo);
+
+  // Manejar la tecla "Enter" para enviar el correo
+  emailInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      enviarCorreo();
+    }
+  });
 }
--- app/main.js
+++ app/main.js
@@ -1,66 +1,144 @@
 window.onload = () => {
+  let startIndex = 0;
+  const articlesPerPage = 32;
+  let totalFilteredArticles = 0;
+  let accumulatedArticles = [];
 
-let startIndex = 0;
-const articlesPerPage = 32;
+  function generateArticles(data, start, end, searchValue) {
+    const articlesContainer = document.getElementById('articles-container');
 
-function generateArticles(data, start, end) {
-  const articlesContainer = document.getElementById('articles-container');
-
-  for (let i = start; i < end; i++) {
-    if (i >= data.articulos.length) {
-      document.getElementById('ver-mas-button').style.display = 'none';
-      break;
+    if (startIndex === 0) {
+      articlesContainer.innerHTML = '';
+      accumulatedArticles = [];
     }
 
-    const articulo = data.articulos[i];
-    const articleDiv = document.createElement('div');
-    articleDiv.classList.add('article');
+    totalFilteredArticles = 0;
 
-    if (articulo.imagenes && articulo.imagenes.length > 0) {
-      const imageUrls = articulo.imagenes.split(',').map(url => url.trim());
-      if (imageUrls.length > 0 && imageUrls[0] !== "") {
-        const img = document.createElement('img');
-        img.src = imageUrls[0]; // Mostrar la primera imagen
-        img.classList.add('article-image');
-        articleDiv.appendChild(img);
+    for (let i = 0; i < data.articulos.length; i++) {
+      const articulo = data.articulos[i];
+      const palabrasClave = articulo.palabras_clave.split(',').map(keyword => keyword.trim().toLowerCase());
+      const shouldShow = palabrasClave.some(keyword => keyword.includes(searchValue.toLowerCase()));
+
+      if (shouldShow) {
+        totalFilteredArticles++;
+
+        if (startIndex === 0 && accumulatedArticles.length < end) {
+          accumulatedArticles.push(articulo);
+        }
+
+        if (totalFilteredArticles > start && totalFilteredArticles <= end) {
+          const articleDiv = document.createElement('div');
+          articleDiv.classList.add('article');
+
+          if (articulo.imagenes && articulo.imagenes.length > 0) {
+            const imageUrls = articulo.imagenes.split(',').map(url => url.trim());
+            if (imageUrls.length > 0 && imageUrls[0] !== "") {
+              const img = document.createElement('img');
+              img.src = imageUrls[0];
+              img.classList.add('article-image');
+              articleDiv.appendChild(img);
+            }
+          }
+
+          if (articulo.titulo && articulo.titulo.trim() !== "") {
+            const title = document.createElement('h2');
+            title.textContent = articulo.titulo;
+            articleDiv.appendChild(title);
+          }
+
+          if (articulo.subtitulo && articulo.subtitulo.trim() !== "") {
+            const subtitle = document.createElement('p');
+            subtitle.textContent = articulo.subtitulo;
+            articleDiv.appendChild(subtitle);
+          }
+
+          articleDiv.addEventListener('click', () => {
+            window.location.href = `article.html?id=${articulo.Id}`;
+          });
+
+          articlesContainer.appendChild(articleDiv);
+        }
       }
     }
 
-    if (articulo.titulo && articulo.titulo.trim() !== "") {
-      const title = document.createElement('h2');
-      title.textContent = articulo.titulo;
-      articleDiv.appendChild(title);
+    if (totalFilteredArticles <= end) {
+      document.getElementById('ver-mas-button').style.display = 'none';
+    } else {
+      document.getElementById('ver-mas-button').style.display = 'flex';
     }
-
-    if (articulo.subtitulo && articulo.subtitulo.trim() !== "") {
-      const subtitle = document.createElement('p');
-      subtitle.textContent = articulo.subtitulo;
-      articleDiv.appendChild(subtitle);
-    }
-
-    articleDiv.addEventListener('click', () => {
-      window.location.href = `article.html?id=${articulo.Id}`;
-    });
-
-    articlesContainer.appendChild(articleDiv);
   }
-}
 
-function loadMoreArticles(data) {
-  const endIndex = startIndex + articlesPerPage;
-  generateArticles(data, startIndex, endIndex);
-  startIndex = endIndex;
-}
+  function loadMoreArticles(data, searchValue) {
+    const endIndex = startIndex + articlesPerPage;
+    generateArticles(data, startIndex, endIndex, searchValue);
+  }
 
   fetch("assets/data/data.json")
     .then(response => response.json())
     .then(data => {
-      loadMoreArticles(data);
+      const searchBar = document.getElementById('search-bar');
+
+      function handleSearch() {
+        const searchValue = searchBar.value.toLowerCase();
+        startIndex = 0;
+        loadMoreArticles(data, searchValue);
+      }
+
+      searchBar.addEventListener('input', handleSearch);
+
+      searchBar.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+          handleSearch();
+        }
+      });
 
       const verMasButton = document.getElementById('ver-mas-button');
       verMasButton.addEventListener('click', () => {
-        loadMoreArticles(data);
+        const searchValue = searchBar.value.toLowerCase();
+        startIndex += articlesPerPage;
+        loadMoreArticles(data, searchValue);
       });
+
+      loadMoreArticles(data, ''); // Cargar artículos por defecto al iniciar
     })
     .catch(error => console.error('Error al cargar los datos:', error));
+
+  // ENVIAR CORREO
+const emailInput = document.getElementById('emailInput');
+const enviarCorreoBtn = document.getElementById('enviarCorreoBtn');
+let mensajeMostrado = false;
+
+function enviarCorreo() {
+  const mensaje = document.getElementById('mensaje');
+  const correo = emailInput.value.trim().toLowerCase();
+  const dominiosPermitidos = ['@esdmadrid.es', '@educamadrid.org', '@hotmail.com', '@gmail.com'];
+
+  let valido = dominiosPermitidos.some(dominio => correo.endsWith(dominio));
+
+  if (valido) {
+    mensaje.textContent = `Correo enviado a ${correo}.`;
+    mensaje.style.color = 'white';
+    emailInput.value = ''; 
+  } else {
+    mensaje.textContent = 'Por favor, ingresa una dirección de correo válida.';
+    mensaje.style.color = 'white';
+  }
+
+  mensaje.style.opacity = '1';
+
+  setTimeout(() => {
+    mensaje.style.opacity = '0';
+    mensaje.style.transition = 'opacity 1s ease-in-out';
+  }, 5000);
+}
+
+enviarCorreoBtn.addEventListener('click', enviarCorreo);
+
+// Manejar la tecla "Enter" para enviar el correo
+emailInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    enviarCorreo();
+  }
+});
+
 }
